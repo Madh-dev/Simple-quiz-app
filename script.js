e80279db2a1f64d39018204ba49ad151620e7410
abdul <aleemabdulmuheez@gmail.com>
2023-03-13 17:10:10 +0100

    okay dey play

--- script.js
+++ script.js
@@ -8,10 +8,361 @@ var opt3 = document.getElementById('opt3')
 var opt4 = document.getElementById('opt4')
 var cA = document.getElementById('cA')
 var totalQuestion = document.getElementById('totalQuestion');
-import { questions } from "./database/q1";
+
 
 var app={
-        questions:questions,
+        questions: [
+            {
+                q:'Which bone is also known as cheek bone',
+                options: [
+                    'Palantine', 
+                'Maxilla',
+                 'Vomer',
+                  'Zygomatic'
+                ],
+                answer:4
+            },
+            {
+                q:'Which test is contraindicated in women with antepartum hemorrhage? ',
+                options: [
+                    'USG abdomen', 
+                'Per Vaginal examination',
+                 'Amniocentesis',
+                  'Chorionic villus sampling'
+                ],
+                answer:2
+            },
+            {
+                q:'Non-Pregnant female uterus weight?',
+                options: [
+                    '60 kg', 
+                '60 lb',
+                 '60 gm',
+                  '60 mg'
+                ],
+                answer:3
+            },
+            {
+                q:'The sequence of steps for neonatal resuscitation',
+                options: [
+                    'ABCT', 
+                'CTAB',
+                 'BACT',
+                  'TABC'
+                ],
+                answer:4
+            },
+            {
+                q:'The last product in the process of blood coagulation is ',
+                options: [
+                    'Fibrin', 
+                'Thrombin',
+                 'Prothrombin',
+                  'Thromboplastin'
+                ],
+                answer:1
+            },
+            {
+                q:'Bluish discoloration of cervix during pregnancy',
+                options: [
+                    'Hegar sign', 
+                'Goodell sign',
+                 'Chadwick sign',
+                  "Jacquemier's sign"
+                ],
+                answer:3
+            },
+            {
+                q:'Acute bleeding from the nose is known as',
+                options: [
+                    'Hematemesis', 
+                'Hemoptysis',
+                 'Epistaxis',
+                  "Rhinorrhoea"
+                ],
+                answer:3
+            },
+            {
+                q:"Cooley's anemia is caused by which deficiency",
+                options: [
+                    'Deficiency of Vit C', 
+                'Deficiency of Vit B12',
+                 'Deficiency of beta-chain of hemoglobin',
+                  "Deficiency of alfa chain"
+                ],
+                answer:3
+            },
+            {
+                q:'The normal intraocular pressure IOP is',
+                options: [
+                    '5-10 mmhg', 
+                '10-21 mmhg',
+                 '20-30 mmhg',
+                  "15-25 mmhg"
+                ],
+                answer:2
+            },
+            {
+                q:'Who coined the term schizophrenia',
+                options: [
+                    'Kraepelin', 
+                'Sander',
+                 'Bleuler',
+                  "Mayer Gross"
+                ],
+                answer:3
+            },
+            {
+                q:'Early signs of hypoxia',
+                options: [
+                    'Restlessness', 
+                'Helplessness',
+                 'Hopelessness',
+                  "Cyanosis"
+                ],
+                answer:1
+            },
+            {
+                q:'The test that is most specific for myocardial damage is?',
+                options: [
+                    'CK MB', 
+                'Myoglobin',
+                 'Troponin 1',
+                  "CK"
+                ],
+                answer:3
+            },
+            {
+                q:'Ramsay Scale is used for the assessment of?',
+                options: [
+                    'Sedation', 
+                'Anxious Patient',
+                 'Pain',
+                  "Arousal"
+                ],
+                answer:1
+            },
+            {
+                q:'The snow storm appearance of the uterus in USG indicates the presence of',
+                options: [
+                    'Ectopic Pregnancy', 
+                'Fibroids in pregnancy',
+                 'Hydramnios',
+                  "Hydatiform mole"
+                ],
+                answer:4
+            },
+            {
+                q:'Megaloblastic anemia is caused due to deficiency of ',
+                options: [
+                    'Cobalamin', 
+                'Pyridoxine',
+                 'Niacin',
+                  "Folic acid"
+                ],
+                answer:4
+            },
+            {
+                q:'Nitroglycerine is administered by which route',
+                options: [
+                    'Sublingual', 
+                'Inhalation',
+                 'Parenteral',
+                  "Insertion"
+                ],
+                answer:1
+            },
+            {
+                q:'Which type of hallucination is most common in schizophrenia ?',
+                options: [
+                    'Visual', 
+                'Gustatory',
+                 'Tactile',
+                  "Auditory"
+                ],
+                answer:4
+            },
+            {
+                q:'The drug of choice in Paracetamol overdose',
+                options: [
+                    'Naloxone', 
+                'Atropine sulphate',
+                 'Neostigmine',
+                  "N- acetyl cysteine"
+                ],
+                answer:4
+            },
+            {
+                q:'Clinical death is also known as ?',
+                options: [
+                    'Brain death', 
+                'Accidental death',
+                 'Legal death',
+                  "Biological death"
+                ],
+                answer:1
+            },
+            {
+                q:'The natural pacemaker of the heart is ?',
+                options: [
+                    'AV node', 
+                'SA node',
+                 'Purkinje Fibre',
+                  "Bundle of His"
+                ],
+                answer:2
+            },
+            {
+                q:'The infant doubles his/her birth weight in which month',
+                options: [
+                    '5', 
+                '6',
+                 '7',
+                  "8"
+                ],
+                answer:2
+            },
+            {
+                q:'Unshakable belief is known as',
+                options: [
+                    'Hallucination', 
+                'Illusion',
+                 'Delusion',
+                  "Perception"
+                ],
+                answer:3
+            },
+            {
+                q:'The most common type of Anemia in pregnancy',
+                options: [
+                    'Iron deficiecy', 
+                'Megaloblastic',
+                 'Pernicious',
+                  "None of these"
+                ],
+                answer:1
+            },
+            {
+                q:'Painless bleeding after 28 weeks of pregnancy is associated with ?',
+                options: [
+                    'Hydatiform mole', 
+                'Placenta previa',
+                 'Placenta abruptio',
+                  "Molar pregnancy"
+                ],
+                answer:2
+            },
+            {
+                q:'The permanent cessation of menstruation is',
+                options: [
+                    'Amenorrhea', 
+                'Menopause',
+                 'Oligomenorrhea',
+                  "Hypomenorrhea"
+                ],
+                answer:2
+            },
+            {
+                q:'which of the following antibodies occurs as pentameric structure ?',
+                options: [
+                    'IgA', 
+                'IgG',
+                 'IgE',
+                  "IgM"
+                ],
+                answer:4
+            },
+            {
+                q:'Drug of choice in bedwetting (Enuresis) ?',
+                options: [
+                    'Lasix', 
+                'Imipramine',
+                 'Diclofenac',
+                  "Amodiaquine"
+                ],
+                answer:2
+            },
+            {
+                q:'The most abundant immunoglobulin in serum is',
+                options: [
+                    'IgA', 
+                    'IgG',
+                     'IgE',
+                      "IgM"
+                ],
+                answer:2
+            },
+             {
+                q:'Short shuffling gait is characteristically seen in',
+                options: [
+                    "Alzheimer's disease", 
+                "Parkinson's disease",
+                 'Multiple sclerosis',
+                  "Mysthenia gravis"
+                ],
+                answer:2
+            },
+            {
+                q:"'After meal' is abbreviated as ?",
+                options: [
+                    'p.r.n', 
+                'p.m',
+                 'p.c',
+                  "h.s"
+                ],
+                answer:3
+            },
+            {
+                q:'Drug of choice for Bipolar Affective Disorder is ?',
+                options: [
+                    'Lithium carbonate', 
+                'Chlorpromazine',
+                 'Bupropion',
+                  "Benzodiazepines"
+                ],
+                answer:1
+            },
+            {
+                q:'Rice water stool is the typical sign of ',
+                options: [
+                    'Cholera', 
+                'Hepatitis A',
+                 'Hepatitis B',
+                  "Dysentery"
+                ],
+                answer:1
+            },
+            {
+                q:'Normal length of newborn at birth is ?',
+                options: [
+                    '20-25cm', 
+                '45-55cm',
+                 '72-80cm',
+                  "81-95cm"
+                ],
+                answer:2
+            },
+            {
+                q:'Major protein digesting enzyme present in the pancreatic juice ?',
+                options: [
+                    'tyrpsin', 
+                'aminopeptidase',
+                 'pepsin',
+                  "dipeptidase"
+                ],
+                answer:1
+            },
+            {
+                q:'Normal serum chloride level is ?',
+                options: [
+                    '135 - 145 meq/L', 
+                '120 - 130 meq/L',
+                 '98 - 107 meq/L',
+                  "50 - 70 meq/L"
+                ],
+                answer:3
+            },
+        ],
         index:0,
         load:function(){
             if(this.index<=this.questions.length-1){
